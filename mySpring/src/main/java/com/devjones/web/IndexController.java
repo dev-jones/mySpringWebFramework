a2e2e6097321010347936461631c1ace4a48f329
dev-jones <twry29@gmail.com>
2020-11-07 21:55:17 +0900

    indexController 추가

---
+++ mySpring/src/main/java/com/devjones/web/IndexController.java
@@ -0,0 +1,12 @@
+package com.devjones.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+	public void alpha() {
+		
+		System.out.println("알파메소드");
+	}
+}
