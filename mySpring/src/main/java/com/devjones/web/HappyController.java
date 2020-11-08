e8ee2fb8a6b1742192f4f023faac6b5a9d17a7ae
dev-jones <twry29@gmail.com>
2020-11-08 15:25:04 +0900

    slf4j 추가

--- mySpring/src/main/java/com/devjones/web/HappyController.java
+++ mySpring/src/main/java/com/devjones/web/HappyController.java
@@ -1,5 +1,7 @@
 package com.devjones.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HappyController {
 
+	private Logger LOG = LoggerFactory.getLogger(this.getClass());	// this.getClass()대신 HappyController.class가능
+	
 	@RequestMapping(value="/happySample.do", method=RequestMethod.GET)
 	public String beHappy() {
 		
-		System.out.println("해피해피~");
+		//System.out.println("해피해피~");
+		LOG.info("해피해피~");
 		
 		return "happySample";
 	}
